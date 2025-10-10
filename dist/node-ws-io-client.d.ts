71430e93eb6579c0d9cd06ae026fd0c7f7b33bba
jgiral-metacodi <65395847+jgiral-metacodi@users.noreply.github.com>
2025-10-10 20:54:50 +0200

    auto-commit

--- dist/node-ws-io-client.d.ts
+++ dist/node-ws-io-client.d.ts
@@ -5,16 +5,24 @@ import { WebsocketIoClientOptions, WsConnection, WsConnectionState } from './nod
 export declare abstract class WebsocketIoClient extends ApiClient {
     options: WebsocketIoClientOptions;
     protected debug: boolean;
+    /** Reference to the client socket opened with the server. */
     socket: Socket<any, any>;
+    /** Connection state. */
     get status(): WsConnectionState;
     set status(value: WsConnectionState);
     private connectionStatus;
     get isConnected(): boolean;
+    /** Notify the current connection state. */
     statusChanged: Subject<WsConnectionState>;
+    /** Initial delay before attempting to reconnect. */
     protected initialReconnectPeriod: number;
+    /** Accumulated delay before attempting to reconnect. */
     protected reconnectPeriod: number;
+    /** Maximum delay before attempting to reconnect. */
     protected maxReconnectPeriod: number;
+    /** Timestamp of the last disconnection. */
     disconnectedAt: string;
+    /** Timeout ID used to verify the reconnection is stable. */
     reconnectingTimeout: NodeJS.Timeout | number;
     constructor(options: WebsocketIoClientOptions);
     abstract get connection(): Promise<WsConnection>;
@@ -24,6 +32,9 @@ export declare abstract class WebsocketIoClient extends ApiClient {
     protected destroy(): void;
     protected onConnect(): void;
     protected onError(error: any): void;
+    /** Fired when the connection between server and client is lost.
+     * {@link https://socket.io/docs/v4/client-socket-instance/#disconnect disconnect: possible reasons}
+     */
     protected onClose(event: string): void;
     protected setReconnectingTimeout(): void;
     protected clearReconnectingTimeout(): void;
--- dist/node-ws-io-client.js
+++ dist/node-ws-io-client.js
@@ -9,6 +9,7 @@ const rxjs_1 = require("rxjs");
 const moment_1 = __importDefault(require("moment"));
 const node_api_client_1 = require("@metacodi/node-api-client");
 class WebsocketIoClient extends node_api_client_1.ApiClient {
+    /** Connection state. */
     get status() { return this.connectionStatus; }
     set status(value) { const old = this.connectionStatus; this.connectionStatus = value; if (old !== value) {
         this.statusChanged.next(value);
@@ -18,13 +19,20 @@ class WebsocketIoClient extends node_api_client_1.ApiClient {
         super(options);
         this.options = options;
         this.debug = false;
+        /** Reference to the client socket opened with the server. */
         this.socket = undefined;
         this.connectionStatus = 'initial';
+        /** Notify the current connection state. */
         this.statusChanged = new rxjs_1.Subject();
+        /** Initial delay before attempting to reconnect. */
         this.initialReconnectPeriod = 5 * 1000;
+        /** Accumulated delay before attempting to reconnect. */
         this.reconnectPeriod = 5 * 1000;
+        /** Maximum delay before attempting to reconnect. */
         this.maxReconnectPeriod = 5 * 60 * 1000;
+        /** Timestamp of the last disconnection. */
         this.disconnectedAt = undefined;
+        /** Timeout ID used to verify the reconnection is stable. */
         this.reconnectingTimeout = undefined;
         this.debug = !!(options === null || options === void 0 ? void 0 : options.local);
         if (this.debug) {
@@ -32,12 +40,16 @@ class WebsocketIoClient extends node_api_client_1.ApiClient {
         }
     }
     async connect() {
+        // Destroy any previous socket instance.
         this.destroy();
+        // Retrieve connection details.
         const { url, path, query } = await this.connection;
+        // Create new socket instance.
         this.socket = (0, socket_io_client_1.io)(url, Object.assign({ path, transports: ['polling'] }, { query }));
         if (this.debug) {
             console.log(this.wsId, '=> connecting', `${url}${path}`);
         }
+        // socket.io events
         this.socket.on('connect', () => this.onConnect());
         this.socket.on('connect_error', (error) => this.onError(error));
         this.socket.on('disconnect', (reason, description) => this.onClose(`socket disconnect ${reason}`));
@@ -88,19 +100,28 @@ class WebsocketIoClient extends node_api_client_1.ApiClient {
             this.socket = undefined;
         }
     }
+    // ---------------------------------------------------------------------------------------------------
+    //  onConnect . onClose . onError
+    // ---------------------------------------------------------------------------------------------------
     onConnect() {
         if (this.debug) {
             console.log(this.wsId, `=> ${this.status === 'reconnecting' ? 'reconnected!' : 'connected!'}`);
         }
+        // Start a short wait period to confirm the connection is stable.
         if (this.status === 'reconnecting') {
             this.setReconnectingTimeout();
         }
+        // Update connection status.
         this.status = 'connected';
+        // Called when the underlying connection is closed.
         this.socket.io.engine.on('close', (reason) => this.onClose(`engine close: ${reason}`));
     }
     onError(error) {
         console.error(`${this.wsId} =>`, error.message);
     }
+    /** Fired when the connection between server and client is lost.
+     * {@link https://socket.io/docs/v4/client-socket-instance/#disconnect disconnect: possible reasons}
+     */
     onClose(event) {
         if (this.debug) {
             console.log(this.wsId, '=> closed', event);
@@ -135,7 +156,10 @@ class WebsocketIoClient extends node_api_client_1.ApiClient {
         }
     }
     setDisconnectionTime() {
+        // Since reconnection may happen multiple times in a row, keep the first timestamp.
         if (!this.disconnectedAt) {
+            // NOTA: Since we don't know exactly when the connection started failing,
+            // we subtract the full timeout period to ensure we fetch pending changes via the audit query.
             const pingTimeout = 20 * 1000;
             this.disconnectedAt = (0, moment_1.default)().subtract(pingTimeout, 'milliseconds').format('YYYY-MM-DD HH:mm:ss');
             if (this.debug) {
@@ -143,6 +167,9 @@ class WebsocketIoClient extends node_api_client_1.ApiClient {
             }
         }
     }
+    // ---------------------------------------------------------------------------------------------------
+    //  log
+    // ---------------------------------------------------------------------------------------------------
     get wsId() { return this.constructor.name; }
 }
 exports.WebsocketIoClient = WebsocketIoClient;
